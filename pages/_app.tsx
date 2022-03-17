79837844a847d1b4fa152463f361441dfc373706
szkyudi <soccer.gk.yudai.s@gmail.com>
2022-03-17 21:20:27 +0900

    Recoilの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import { Global } from '@emotion/react'
+import { RecoilRoot } from 'recoil'
 import { globalStyles } from '../shared/styles'
 
 const App = ({ Component, pageProps }) => (
-  <>
+  <RecoilRoot>
     <Global styles={globalStyles} />
     <Component {...pageProps} />
-  </>
+  </RecoilRoot>
 )
 
 export default App
