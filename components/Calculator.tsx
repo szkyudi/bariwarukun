bb531f430982b8f83523bdf9011fe4c7ec327f22
szkyudi <soccer.gk.yudai.s@gmail.com>
2022-03-28 10:45:30 +0900

    update: デザインの修正

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, Divider, FormControl, FormControlLabel, FormLabel, Grid, IconButton, InputLabel, List, ListItem, MenuItem, Paper, Radio, RadioGroup, Select, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
+import { Alert, Box, Button, ButtonGroup, Divider, FormControl, FormControlLabel, FormLabel, Grid, IconButton, InputLabel, List, ListItem, MenuItem, Paper, Radio, RadioGroup, Select, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
 import { FormEvent, useState } from "react"
 import { NumericInput } from "./NumericInput";
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -66,111 +66,12 @@ export const Calculator = () => {
 
  return (
     <>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell component="th" scope="row" sx={{py: 1}}>
-                お会計
-              </TableCell>
-              <TableCell align="right">
-                {totalBill || 0} 円
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell component="th" scope="row" sx={{py: 1}}>
-                お預かり
-              </TableCell>
-              <TableCell align="right">
-                {totalPay || 0} 円
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell component="th" scope="row" sx={{py: 1}}>
-                {remaining >= 0 ? "お釣り" : "不足金額"}
-              </TableCell>
-              <TableCell align="right">
-                {remaining >= 0 ? remaining || 0 : -remaining || 0} 円
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TableContainer sx={{mt: 1}} component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{py: 1}} variant="head">
-                  支払い金額
-              </TableCell>
-              <TableCell sx={{py: 1}} variant="head" align="right">
-                  人数
-              </TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell sx={{py: 1}}>
-                {generalBill || 0} 円
-              </TableCell>
-              <TableCell sx={{py: 1}} align="right">
-                {generalPayerNum || 0} 人
-              </TableCell>
-              <TableCell sx={{width: 136, py: 1}} align="right">
-                <FormControl sx={{width: 110}}>
-                  <InputLabel id="ceil-unit-label">丸め単位</InputLabel>
-                  <Select
-                    labelId="ceil-unit-label"
-                    id="ceil-unit"
-                    value={ceilUnit}
-                    label="丸め単位"
-                    size="small"
-                    onChange={(e) => setCeilUnit(Number(e.target.value))}
-                  >
-                    <MenuItem value={1}>1円</MenuItem>
-                    <MenuItem value={10}>10円</MenuItem>
-                    <MenuItem value={100}>100円</MenuItem>
-                    <MenuItem value={1000}>1000円</MenuItem>
-                  </Select>
-                </FormControl>
-              </TableCell>
-            </TableRow>
-            {payOptions.map(option => (
-              <TableRow key={option.id}>
-                <TableCell sx={{py: 1}}>
-                  {option.bill || '- '} 円
-                </TableCell>
-                <TableCell sx={{py: 1}} align="right">
-                  {option.payerNum || '- '} 人
-                </TableCell>
-                <TableCell sx={{width: 136, py: 1}} align="right">
-                  <ButtonGroup variant="text" size="small">
-                    <Button
-                      onClick={() => decrementOptionPayerNum(option.id)}
-                    >
-                      <RemoveIcon />
-                    </Button>
-                    <Button
-                      onClick={() => incrementOptionPayerNum(option.id)}
-                      disabled={generalPayerNum <= 0}
-                    >
-                      <AddIcon />
-                    </Button>
-                    <Button
-                      color="error"
-                      onClick={() => removeOption(option.id)}
-                    >
-                      <DeleteIcon />
-                    </Button>
-                  </ButtonGroup>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Grid container spacing={1} sx={{mt: 2}}>
+      <Alert severity="success">
+        <Typography variant="caption">
+          リアルタイムで変更を検知して再計算されます
+        </Typography>
+      </Alert>
+      <Grid container spacing={1} sx={{mt: 1}}>
         <Grid item xs={7}>
           <NumericInput
             label="お会計"
@@ -183,7 +84,7 @@ export const Calculator = () => {
         </Grid>
         <Grid item xs={5}>
           <NumericInput
-            label="支払い人数"
+            label="合計人数"
             value={totalPayerNum || ''}
             setter={setTotalPayerNum}
             adorment="人"
@@ -192,48 +93,150 @@ export const Calculator = () => {
           />
         </Grid>
       </Grid>
-      <Divider sx={{my: 1}}>
-        <Typography variant="caption">
-          個別の支払い金額の追加
-        </Typography>
-      </Divider>
-      <form onSubmit={handleSubmit}>
-        <Grid container spacing={1}>
-          <Grid item xs={7}>
-            <NumericInput
-              label="金額"
-              adorment="円"
-              value={optionBill || ''}
-              setter={setOptionBill}
-              size="small"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={5}>
-            <NumericInput
-              label="人数"
-              adorment="人"
-              value={optionPayerNum || ''}
-              setter={setOptionPayerNum}
-              size="small"
-              fullWidth
-            />
-          </Grid>
-        </Grid>
-        <Stack>
-          <Button
-            type="submit"
-            sx={{mt: 1}}
-            variant="outlined"
-            size="small"
-            disabled={optionBill === 0 || optionPayerNum === 0}
-          >
-            <Typography variant="button">
-              追加
-            </Typography>
-          </Button>
-        </Stack>
-      </form>
+      <Paper sx={{mt: 1}}>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell size="small" variant="head">
+                    <Typography variant="caption">
+                      支払い金額
+                    </Typography>
+                </TableCell>
+                <TableCell size="small" variant="head" align="right">
+                    <Typography variant="caption">
+                      人数
+                    </Typography>
+                </TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell sx={{py: 1}}>
+                  {generalBill || 0} 円
+                </TableCell>
+                <TableCell sx={{py: 1}} align="right">
+                  {generalPayerNum || 0} 人
+                </TableCell>
+                <TableCell sx={{width: 136, py: 1}} align="right">
+                  <FormControl sx={{width: 110}}>
+                    <InputLabel id="ceil-unit-label">丸め単位</InputLabel>
+                    <Select
+                      labelId="ceil-unit-label"
+                      id="ceil-unit"
+                      value={ceilUnit}
+                      label="丸め単位"
+                      size="small"
+                      onChange={(e) => setCeilUnit(Number(e.target.value))}
+                    >
+                      <MenuItem value={1}>1円</MenuItem>
+                      <MenuItem value={10}>10円</MenuItem>
+                      <MenuItem value={100}>100円</MenuItem>
+                      <MenuItem value={1000}>1000円</MenuItem>
+                    </Select>
+                  </FormControl>
+                </TableCell>
+              </TableRow>
+              {payOptions.map(option => (
+                <TableRow key={option.id}>
+                  <TableCell sx={{py: 1}}>
+                    {option.bill || '- '} 円
+                  </TableCell>
+                  <TableCell sx={{py: 1}} align="right">
+                    {option.payerNum || '- '} 人
+                  </TableCell>
+                  <TableCell sx={{width: 136, py: 1}} align="right">
+                    <ButtonGroup variant="text" size="small">
+                      <Button
+                        onClick={() => decrementOptionPayerNum(option.id)}
+                      >
+                        <RemoveIcon />
+                      </Button>
+                      <Button
+                        onClick={() => incrementOptionPayerNum(option.id)}
+                        disabled={generalPayerNum <= 0}
+                      >
+                        <AddIcon />
+                      </Button>
+                      <Button
+                        color="error"
+                        onClick={() => removeOption(option.id)}
+                      >
+                        <DeleteIcon />
+                      </Button>
+                    </ButtonGroup>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box sx={{p: 2}}>
+          <Typography variant="caption">
+            多めや少なめに払う人たちの追加
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={1}>
+              <Grid item xs={7}>
+                <NumericInput
+                  label="金額"
+                  adorment="円"
+                  value={optionBill || ''}
+                  setter={setOptionBill}
+                  size="small"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <NumericInput
+                  label="人数"
+                  adorment="人"
+                  value={optionPayerNum || ''}
+                  setter={setOptionPayerNum}
+                  size="small"
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+            <Stack>
+              <Button
+                type="submit"
+                sx={{mt: 1}}
+                variant="contained"
+                size="small"
+                disabled={optionBill === 0 || optionPayerNum === 0}
+              >
+                <Typography variant="button">
+                  追加
+                </Typography>
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Paper>
+      <TableContainer sx={{mt: 1}}>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell size="small" component="th" scope="row">
+                集まる金額
+              </TableCell>
+              <TableCell size="small" align="right">
+                {totalPay || 0} 円
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell size="small" component="th" scope="row">
+                {remaining >= 0 ? "お釣り" : "不足金額"}
+              </TableCell>
+              <TableCell size="small" align="right">
+                {remaining >= 0 ? remaining || 0 : -remaining || 0} 円
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
